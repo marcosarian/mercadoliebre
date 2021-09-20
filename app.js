4b7d0245c476081f6fc400190cb706338f764548
marcos <marcosarian@hotmail.com>
2021-09-19 22:08:13 -0300

    mercado

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ const app=express();
 app.use(express.static('public'));
 
 
-app.listen(3007,()=>{console.log(" servidor corriendo")});
+app.listen(3007, function(){console.log(" servidor corriendo")});
 
 app.get('/', (req,res)=>{res.sendFile (path.join (__dirname,"view/home.html"))})
 app.get('/login', (req,res)=>{res.sendFile (path.join (__dirname,"view/login.html"))})
+app.get('/register', (req,res)=>{res.sendFile (path.join (__dirname,"view/register.html"))})
+
